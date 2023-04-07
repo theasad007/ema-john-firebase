6011874af292ae73aab34249f134a7096ef23d1d
Asaduzzaman Ali <theasad.dev@gmail.com>
2023-04-08 02:42:54 +0600

    cart added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,17 @@ const Cart = (props) => {
     let total = 0;
     let totalShipping = 0;
     let tax = 10;
+    let quantity = 0;
     for(const product of cart) {
-        total = total + product.price;
-        totalShipping = totalShipping + product.shipping;
+        // long Hand
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+        // Short Hand
+        // product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping * product.quantity;
+        quantity = quantity + product.quantity
     }
 
     const totalTax = total * tax / 100;
@@ -19,7 +27,7 @@ const Cart = (props) => {
         <div className="cart-wrapper">
             <div className="cart-container">
                 <h4>Order Summary</h4>
-                <p>Selected Items: {cart.quantity}</p>
+                <p>Selected Items: {quantity}</p>
                 <p>Total Price: ${total}</p>
                 <p>Total Shipping: ${totalShipping}</p>
                 <p>Tax:${totalTax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -36,7 +36,24 @@ const Shop = () => {
 
     const handleAddToCart = (product) => {
         // cart.push(product); 
-        const newCart = [...cart, product];
+
+        // const newCart = [...cart, product];
+        // // newCart optional
+
+        let newCart = [];
+        // if not exist then set quantity 1
+        // if exist update quantity by 1
+        const exists = cart.find(pd => pd.id === product.id);
+        if(!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists]
+        }
+
         setCart(newCart);
         addToDb(product.id);
     }
